06af9facbca85fc8abc4a49e4b67a052a0042182
Josue Rodriguez <maybejosue@gmail.com>
2021-05-05 10:15:42 -0700

    • Persists state in index.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import React from 'react';
-import './App.css';
-import {connect} from 'react-redux'
-import {Switch, Route} from 'react-router-dom'
-import PrivateRoute from './utils/PrivateRoute'
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
 
-import Navigation from './components/Navigation'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import SellerDash from './components/SellerDash'
-import BidderDash from './components/BidderDash'
-import AuctionList from './components/AuctionList'
-import CloserLook from './components/CloserLook'
+import Navigation from "./components/Navigation";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import SellerDash from "./components/SellerDash";
+import BidderDash from "./components/BidderDash";
+import AuctionList from "./components/AuctionList";
+import CloserLook from "./components/CloserLook";
 
 function App(props) {
-  console.log(`in app.js`,props)
+  console.log(`in app.js`, props);
   return (
     <div className="App">
       <Switch>
-      <Route exact path='/signup'>
-          <Signup/>
-      </Route>
-      <Route exact path='/login'>
-        <Login/>
-      </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
       </Switch>
-      <Navigation/>
-            <Switch>
-                <PrivateRoute exact path='/auctions' component={AuctionList}/>
-                <PrivateRoute exact path='/dashboard/seller/:id' component={SellerDash}/>
-                <PrivateRoute exact path='/dashboard/bidder/:id' component={BidderDash}/>
-                <PrivateRoute exact path='/closerlook/:id' component={CloserLook}/>
-            </Switch>
+      <Navigation />
+      <Switch>
+        <PrivateRoute exact path="/auctions" component={AuctionList} />
+        <PrivateRoute
+          exact
+          path="/dashboard/seller/:id"
+          component={SellerDash}
+        />
+        <PrivateRoute
+          exact
+          path="/dashboard/bidder/:id"
+          component={BidderDash}
+        />
+        <PrivateRoute exact path="/closerlook/:id" component={CloserLook} />
+      </Switch>
     </div>
   );
 }
 
-export default connect(state=>{
-  return{}
-},{})(App);
+export default connect((state) => {
+  return {};
+}, {})(App);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Redux
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
-import combineReducer from './reducer/rootReducer'
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import combineReducer from "./reducer/rootReducer";
 
-const store = createStore(combineReducer, applyMiddleware(thunk))
+function saveToLovalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.log(err);
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+const store = createStore(
+  combineReducer,
+  persistedState,
+  applyMiddleware(thunk)
+);
+
+store.subscribe(() => saveToLovalStorage(store.getState()));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
